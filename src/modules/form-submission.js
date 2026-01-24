99499ebaf236028191f145c5e393c0514b2756a1
Mykelsown <mykelsown@gmail.com>
2026-01-24 03:22:02 +0100

    Updated the form and removed the throw error from the submit handler

--- src/modules/form-submission.js
+++ src/modules/form-submission.js
@@ -1,30 +1,34 @@
 export default function submitFormData() {
   const form = document.querySelector('form[name="contact"]');
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const message = document.getElementById("message");
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const formData = new FormData(form);
 
-    if (name.value === "" || email.value === "" || message.value === "") {
-      alert("Please fill the form before submiting");
-      throw new Error("Input field empty");
-    } else {
-      try {
-        await fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        });
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const message = formData.get("message");
 
-        form.reset();
-        alert("Message sent successfully! 🚀");
-      } catch (error) {
-        alert("Something went wrong. Please try again.");
-      }
+    // Validation check
+    if (!name || !email || !message) {
+      alert("Please fill the form before submitting");
+      return;
+    }
+
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      form.reset();
+      alert("Message sent successfully! 🚀");
+    } catch (error) {
+      alert("Something went wrong. Please try again.");
     }
   });
 }
